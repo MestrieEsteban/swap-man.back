47c82017ac1eb07f02c6bdee6451e9e955b64d50
Alexis3490 <alexispiraux72@gmail.com>
2021-05-26 11:13:13 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express()
 
 const io = socketIO(app, {
 	cors: {
-		origin: "https://swapmanfront.herokuapp.com/",
+		origin: "*",
 		methods: ["GET", "POST"],
 		transports: ['websocket', 'polling'],
 		credentials : true
